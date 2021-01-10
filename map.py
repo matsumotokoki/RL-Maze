9da57e1f69a5e52507b371c9322f309a99d0715d
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2021-01-11 03:41:29 +0900

    initial commit

---
+++ map.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Map(object):
+    def __init__(self):
+        self.map = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,],
+                             [0,1,1,1,1,1,1,1,1,1,3,0,],
+                             [0,1,0,0,1,0,0,0,1,1,1,0,],
+                             [0,1,0,0,1,1,0,1,1,1,1,0,],
+                             [0,1,1,1,1,1,1,1,1,1,1,0,],
+                             [0,1,1,1,1,1,1,1,0,0,0,0,],
+                             [0,1,1,0,0,0,1,1,0,1,1,0,],
+                             [0,1,1,0,1,1,1,1,1,1,1,0,],
+                             [0,1,1,1,1,1,1,1,0,0,1,0,],
+                             [0,1,1,1,1,0,0,1,1,0,0,0,],
+                             [0,2,1,1,0,0,1,1,1,1,1,0,],
+                             [0,0,0,0,0,0,0,0,0,0,0,0,]]) 
+        self.size = self.map.shape[0]
+        self.init_pos = [1,1]
+        self.goal_pos = [10,10]
+        plt.figure(figsize=(7,7))
+
+    def chack_movable(self,pos):
+        up    = bool(self.map[11-pos[1]-1][pos[0]])
+        down  = bool(self.map[11-pos[1]+1][pos[0]])
+        right = bool(self.map[11-pos[1]][pos[0]+1])
+        left  = bool(self.map[11-pos[1]][pos[0]-1])
+
+        return([up,down,right,left])
+
+    def plot(self,pos=[1,1],q_table=[]):
+        gpoint = np.arange(0, self.size, 1)
+        plt.vlines(gpoint, 0, self.size, linewidth=0.3, colors="k")
+        plt.hlines(gpoint, 0, self.size, linewidth=0.3, colors="k")
+        plt.xlim(0, self.size)
+        plt.ylim(0, self.size)
+        for i in range (self.size):
+            for j in range (self.size):
+                if self.map[11-j][i] == 0:
+                    plt.axvspan(xmin=i, xmax=i+1, ymin=j/self.size, ymax=(j+1)/self.size, color = "k", alpha=0.9)
+                elif self.map[11-j][i] == 1:
+                    if q_table[i*12+j][np.argmax(q_table[i*12+j])] > 1.0:
+                        plt.axvspan(xmin=i, xmax=i+1, ymin=j/self.size, ymax=(j+1)/self.size, color = "y", \
+                                alpha=np.clip(q_table[i*12+j][np.argmax(q_table[i*12+j])]/100,0,0.9))
+                        if np.argmax(q_table[i*12+j]) == 0: 
+                            plt.text(i, j, "↑", size=30, alpha=np.clip(q_table[i*12+j][np.argmax(q_table[i*12+j])]/100,0,0.9))
+                        elif np.argmax(q_table[i*12+j]) == 1: 
+                            plt.text(i, j, "↓", size=30, alpha=np.clip(q_table[i*12+j][np.argmax(q_table[i*12+j])]/100,0,0.9))
+                        elif np.argmax(q_table[i*12+j]) == 2: 
+                            plt.text(i, j, "→", size=30, alpha=np.clip(q_table[i*12+j][np.argmax(q_table[i*12+j])]/100,0,0.9))
+                        elif np.argmax(q_table[i*12+j]) == 3: 
+                            plt.text(i, j, "←", size=30, alpha=np.clip(q_table[i*12+j][np.argmax(q_table[i*12+j])]/100,0,0.9))
+                        else:pass
+                    else:
+                        pass
+                elif self.map[11-j][i] == 2:
+                    plt.axvspan(xmin=i, xmax=i+1, ymin=j/self.size, ymax=(j+1)/self.size, color = "g", alpha=0.5)
+                elif self.map[11-j][i] == 3:
+                    plt.axvspan(xmin=i, xmax=i+1, ymin=j/self.size, ymax=(j+1)/self.size, color = "b", alpha=0.5)
+                else:
+                    pass
+        plt.axvspan(xmin=pos[0], xmax=pos[0]+1, ymin=pos[1]/self.size, ymax=(pos[1]+1)/self.size,\
+                    color = "r", alpha=0.5)
+        plt.plot()
+        plt.pause(0.0001)
+        plt.cla()
+ 
+
+if __name__ == "__main__":
+    Map().plot()
---
+++ q-learning.py
@@ -0,0 +1,66 @@
+import numpy as np
+from map import Map
+from agent import Agent
+
+class q_learning(object):
+    def __init__(self):
+        self.map = Map()
+        self.agent = Agent()
+        self.max_episode = 100
+        self.steps = 300
+        self.gamma = 0.9
+        self.alpha = 0.8
+        self.q_table = np.random.uniform(low=-1,high=1,\
+                                    size=(self.map.size**2,self.agent.action_space))
+
+    def decide_action(self,next_state,episode,q_table):
+        first_probability = 0.75
+        epsilon = first_probability * (1/(episode+1))
+        if epsilon <= np.random.uniform(0,1):
+            next_action = np.argmax(q_table[next_state])   
+        else:
+            prob = sum(q_table[next_state]+100) 
+            w = (q_table[next_state]+100) / prob
+            next_action = np.random.choice(range(4) ,p=w)  
+        return next_action
+
+    def update_Qtable(self,q_table,state,action,reward,next_state):
+        next_max_q = max(q_table[next_state])
+        q_table[state,action] = (1 - self.alpha) * q_table[state,action] \
+                                + self.alpha * (reward + self.gamma * next_max_q)
+        return q_table
+
+    def reward(self,done,state,next_state):
+        if done:
+            reward = 100
+        elif state == next_state:
+            reward = -10
+        else:
+            reward = -0.1
+        return reward
+
+
+    def run(self):
+        for episode in range(self.max_episode):
+            self.agent = Agent(self.map.init_pos)
+            state = self.agent.get_state()
+            action = np.argmax(self.q_table[state]) 
+            reward_of_episode = 0
+
+            for i in range(self.steps):
+                direction = self.map.chack_movable(self.agent.pos) 
+                self.agent.action(action,direction)
+                done = self.agent.check_done()
+                next_state = self.agent.get_state()
+                reward = self.reward(done,state,next_state) 
+                reward_of_episode += reward
+                self.q_table = self.update_Qtable(self.q_table,state,action,reward,next_state)
+                action = self.decide_action(next_state,episode,self.q_table)
+                state = next_state
+                self.map.plot(self.agent.pos,self.q_table)
+                if done:
+                    break
+            print("episode %5d, reward %6d, step %5d" %(episode+1,reward_of_episode,i+1))
+
+if __name__ == "__main__":
+   q_learning().run() 
